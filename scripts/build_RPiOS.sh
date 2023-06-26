d9e75b38a103c3b3989fb16036e421a804d43498
gearhead <ys3al35l@gmail.com>
2023-06-26 17:34:38 -0500

    Commit to add RPiOS as an OS base for Rune. Arch dropped armv6 in January 2022 (RPi B,B+,Zero) Using RPiOS allows a single 32 bit OS (armhf) image for all Runes: B thru 4B. In addiiton, an arm64 version can also be built which runs on all 64 bit capable Pis (2B rev2, 3B, 3B+, 4B, Zero2W)

---
+++ scripts/build_RPiOS.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+# Script to build PKGs
+# Go to packages directory
+cd ../rpios_packages
+echo "alac"
+cd libalac
+makedeb -s --no-confirm
+rm -rf src/ pkg/ alac/
+echo "bluez-alsa"
+cd ../bluez-alsa
+makedeb -s --no-confirm
+rm -rf src/ pkg/ bluez-alsa-git/
+echo "bluez-alsa-monitor"
+cd ../bluez-alsa-monitor
+makedeb -s --no-confirm
+rm -rf src/ pkg/ bluealsa-monitor/
+echo "ashuffle"
+cd ../ashuffle
+makedeb -s --no-confirm
+rm -rf src/ pkg/ ashuffle/
+echo "connman"
+cd ../connman
+makedeb -s --no-confirm
+rm -rf src/ pkg/
+echo "ell"
+cd ../ell
+makedeb -s --no-confirm
+rm -rf src/ pkg/
+echo "iwd"
+cd ../iwd
+makedeb -s --no-confirm
+rm -rf src/ pkg/
+echo "nginx"
+cd ../nginx-rune
+makedeb -s --no-confirm
+rm -rf src/ pkg/
+echo "ply-lite"
+cd ../ply-lite
+makedeb -s --no-confirm
+rm -rf src/ pkg/ ply-lite/
+echo "openaptx"
+cd ../openaptx
+makedeb -s --no-confirm
+rm -rf src/ pkg/ libopenaptx/
+echo "mpd"
+cd ../mpd
+makedeb -s --no-confirm
+rm -rf src/ pkg/
+echo "shairport-sync"
+cd ../shairport-sync
+makedeb -s --no-confirm
+rm -rf src/ pkg/
+echo "nqptp"
+cd ../nqptp
+makedeb -s --no-confirm
+rm -rf src/ pkg/ nqptp/
+echo "librespot"
+cd ../librespot
+makedeb -s --no-confirm
+rm -rf src/ pkg/ librespot/
+echo "spotifyd"
+cd ../spotifyd
+makedeb -s --no-confirm
+rm -rf src/ pkg/ spotifyd/
+echo "Done with RPiOS RuneAudio packages!"
